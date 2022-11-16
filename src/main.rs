7aa167481ddc3fb94ea81822a955d8f80b6066d6
Declan Li-Carney <declan.li.carney@meetkai.com>
2022-11-16 12:41:56 -0500

    wip moving to desktop

--- src/main.rs
+++ src/main.rs
@@ -1,96 +1,84 @@
-use std::{cell::RefCell, rc::Rc};
+use std::rc::Rc;
+use std::sync::Arc;
 
-use gloo_console::log;
-use gloo_utils::window;
-use js_sys::{Boolean, Object, Reflect};
-use wasm_bindgen::{prelude::Closure, JsCast, JsValue, UnwrapThrowExt};
-use wasm_bindgen_futures::{spawn_local, JsFuture};
-use web_sys::{
-    HtmlCanvasElement, WebGl2RenderingContext, WebGlFramebuffer, XrFrame, XrRenderStateInit,
-    XrSession, XrSessionMode, XrWebGlLayer,
+use bevy::{
+    prelude::*,
+    render::renderer::RenderDevice,
+    webxr::{Canvas, WebXrContext},
 };
+use gloo_console as console;
+use wasm_bindgen_futures::spawn_local;
 
 pub fn main() {
     console_error_panic_hook::set_once();
+    console::log!("main");
     spawn_local(async {
-        start().await.unwrap_throw();
+        start().await;
     });
 }
 
-pub async fn start() -> Result<(), JsValue> {
-    let window = window();
-    let navigator = window.navigator();
-    let xr_system = navigator.xr();
-    let session_supported =
-        JsFuture::from(xr_system.is_session_supported(XrSessionMode::ImmersiveVr))
-            .await?
-            .dyn_into::<Boolean>()?
-            .value_of();
-    {
-        log!("supported", session_supported)
-    };
+pub async fn start() {
+    console::log!("start");
+    let mut app = App::new();
 
-    let session: XrSession = JsFuture::from(xr_system.request_session(XrSessionMode::ImmersiveVr))
-        .await?
-        .dyn_into::<XrSession>()?;
-
-    let document = window.document().unwrap();
-
-    let canvas = document
-        .get_element_by_id("canvas")
-        .unwrap()
-        .dyn_into::<HtmlCanvasElement>()
+    let webxr_context = WebXrContext::get_context(bevy::xr::XrSessionMode::ImmersiveVR)
+        .await
         .unwrap();
 
-    let gl_attribs = Object::new();
+    let gl = webxr_context.canvas.create_webgl2_context();
 
-    Reflect::set(
-        &gl_attribs,
-        &JsValue::from_str("xrCompatible"),
-        &JsValue::TRUE,
+    let mut state = web_sys::XrRenderStateInit::new();
+
+    let base_layer = web_sys::XrWebGlLayer::new_with_web_gl2_rendering_context(
+        &webxr_context.session.borrow(),
+        &gl,
     )
     .unwrap();
 
-    let gl = Rc::new(
-        canvas
-            .get_context_with_context_options("webgl2", &gl_attribs)?
-            .unwrap()
-            .dyn_into::<WebGl2RenderingContext>()?,
-    );
-
-    let mut state = XrRenderStateInit::new();
-
-    let base_layer = XrWebGlLayer::new_with_web_gl2_rendering_context(&session, &gl)?;
-
     state.base_layer(Some(&base_layer));
 
-    session.update_render_state_with_state(&state);
-
-    //Draw
-    type XrFrameHandler = Closure<dyn FnMut(f64, XrFrame)>;
-    let session = Rc::new(RefCell::new(session));
-    let f: Rc<RefCell<Option<XrFrameHandler>>> = Rc::new(RefCell::new(None));
-    let g = f.clone();
-    let closure_session = session.clone();
-    *g.borrow_mut() = Some(Closure::new(move |_time: f64, _frame: XrFrame| {
-        let session = closure_session.borrow();
-
-        let base_layer = session.render_state().base_layer().unwrap();
-        //Reflect hack
-        let framebuffer: WebGlFramebuffer =
-            js_sys::Reflect::get(&base_layer, &"framebuffer".into())
-                .unwrap()
-                .into();
-        gl.bind_framebuffer(WebGl2RenderingContext::FRAMEBUFFER, Some(&framebuffer));
-        gl.clear_color(1.0, 1.0, 0.0, 1.0);
-        gl.clear(
-            WebGl2RenderingContext::COLOR_BUFFER_BIT | WebGl2RenderingContext::DEPTH_BUFFER_BIT,
-        );
-        session.request_animation_frame(f.borrow().as_ref().unwrap().as_ref().unchecked_ref());
-    }));
-
-    session
+    webxr_context
+        .session
         .borrow()
-        .request_animation_frame(g.borrow().as_ref().unwrap().as_ref().unchecked_ref());
-    Ok(())
+        .update_render_state_with_state(&state);
+
+    // WGpu Setup
+    let instance = wgpu::Instance::new(wgpu::Backends::GL);
+
+    let surface = unsafe { instance.create_surface(&webxr_context.canvas) };
+
+    let adapter = instance
+        .request_adapter(&wgpu::RequestAdapterOptions {
+            power_preference: wgpu::PowerPreference::HighPerformance,
+            force_fallback_adapter: false,
+            compatible_surface: Some(&surface),
+        })
+        .await
+        .expect("No suitable GPU adapters found on the system!");
+
+    let (device, queue) = adapter
+        .request_device(
+            &wgpu::DeviceDescriptor {
+                label: Some("device"),
+                features: adapter.features(),
+                limits: adapter.limits(),
+            },
+            None,
+        )
+        .await
+        .expect("Unable to find a suitable GPU adapter!");
+    let adapter_info = adapter.get_info();
+
+    app.world
+        .insert_resource(RenderDevice::from(Arc::new(device)));
+    app.world.insert_resource(Arc::new(queue));
+    app.world.insert_resource(adapter_info);
+    app.world.insert_non_send_resource(webxr_context);
+    app.add_plugins(DefaultPlugins);
+    app.add_system(running_test);
+    app.run();
+}
+
+fn running_test() {
+    bevy::log::info!("Hiii!!!!")
 }
